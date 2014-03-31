f5a1f8bb0ecd34598fccfd5cb55a2583050cb880
Christian Kuepper <wasser@chello.at>
2014-03-31 22:12:38 +0200

    Initial Commit

---
+++ AlphaTouch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlphaTouch
+//
+//  Created by Christian Kuepper on 31/03/14.
+//  Copyright (c) 2014 Christian Kuepper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by Christian Kuepper on 31/03/14.
+//  Copyright (c) 2014 Christian Kuepper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
